8d90b883cf4b4643aa875a51676c06090c2aeec6
Dennis Martinez <dennis@dennmart.com>
2015-01-10 21:23:49 -0800

    Added specs for SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
+  let(:user) { FactoryGirl.create(:user) }
 
+  describe "#create" do
+    it "sets the user ID in the session" do
+      expect(User).to receive(:from_omniauth).and_return(user)
+      get :create, "provider"=>"trello", oauth_token: "token", oauth_verifier: "verifier"
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it "redirects to root" do
+      expect(User).to receive(:from_omniauth).and_return(user)
+      get :create, "provider"=>"trello", oauth_token: "token", oauth_verifier: "verifier"
+      expect(response).to redirect_to(root_url)
+    end
+  end
+
+  describe "#destroy" do
+    before(:each) do
+      session[:user_id] = user.id
+    end
+
+    it "clears the user ID in the session" do
+      get :destroy
+      expect(session[:user_id]).to be_nil
+    end
+
+    it "redirects to root" do
+      get :destroy
+      expect(response).to redirect_to(root_url)
+    end
+  end
 end
