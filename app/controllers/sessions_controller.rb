94d777995f2a42081c399e4a2c01844fda759afb
Dennis Martinez <dennis@dennmart.com>
2015-01-16 12:19:33 -0800

    Added home page and fixing redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to boards_path, :notice => "Signed in!"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
+    redirect_to root_path, :notice => "Signed out!"
   end
 end
