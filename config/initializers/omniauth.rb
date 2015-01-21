ee1effd67c2959ac81993692d11409a1cb1d75f0
Dennis Martinez <dennis@dennmart.com>
2015-01-21 15:06:09 -0800

    Renamed project to Trello Echo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :trello, ENV['TRELLO_KEY'], ENV['TRELLO_SECRET'],
-    app_name: "Trello Saisan", scope: 'read,write', expiration: 'never'
+    app_name: "Trello Echo", scope: 'read,write', expiration: 'never'
 end
