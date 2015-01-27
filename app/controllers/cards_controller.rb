52f459b590226ffa874e19e44a684170a849dba8
Dennis Martinez <dennis@dennmart.com>
2015-01-26 20:17:36 -0800

    Update card information properly

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,6 +13,20 @@ class CardsController < ApplicationController
     @board = @trello.board(@card.trello_board_id, { lists: 'open', cards: 'open' })
   end
 
+  def update
+    @card = current_user.cards.find(params[:id])
+    if @card.update_attributes(card_params)
+      @card.set_next_run
+      flash[:notice] = 'Your card was updated!'
+      redirect_to card_path(@card)
+    else
+      flash[:error] = 'Your card was not updated.'
+      @trello = TrelloApi.new(current_user.oauth_token)
+      @board = @trello.board(@card.trello_board_id, { lists: 'open', cards: 'open' })
+      render :show
+    end
+  end
+
   def destroy
     @card = current_user.cards.where(id: params[:id]).first
     if @card
@@ -26,5 +40,11 @@ class CardsController < ApplicationController
     @card = current_user.cards.where(id: params[:id]).first
     @card.toggle!(:disabled) if @card
     redirect_to cards_path
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title, :description, :trello_board_id, :trello_list_id, :frequency, :frequency_period)
   end
 end
