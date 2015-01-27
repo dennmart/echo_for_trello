48f3039989fe3154d6bb0018800bb3d460cd2cc7
Dennis Martinez <dennis@dennmart.com>
2015-01-27 14:40:39 -0800

    Don't trigger Sidekiq during tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'webmock/rspec'
+require 'sidekiq/testing'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
