36cf5196271437ba54d36a770ee6dfcb2d8e8700
Dennis Martinez <dennis@dennmart.com>
2015-01-31 11:07:59 -0800

    Added custom job type for whenever to run Ruby from rbenv in cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+job_type :rbenv_runner, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && bin/rails runner -e :environment ':task' :output"
+
 every 1.day, :at => '12:01 am' do
-  runner "Card.create_pending_trello_cards"
+  rbenv_runner "Card.create_pending_trello_cards"
 end
