c8d7007c8c18c5593cf0dc11578212bb323859c5
Dennis Martinez <dennis@dennmart.com>
2015-02-20 10:13:20 -0800

    Added footer to application layout and fixed redirect when logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
-  before_filter :redirect_if_authenticated
+  before_filter :redirect_if_authenticated, only: [:home]
   layout 'static'
 
   private
