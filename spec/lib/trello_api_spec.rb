30e95f015cf25b77e30601aac31a55716d7d6976
Dennis Martinez <dennis@dennmart.com>
2015-08-10 12:30:02 -0700

    Finished writing pending specs

--- spec/lib/trello_api_spec.rb
+++ spec/lib/trello_api_spec.rb
@@ -30,10 +30,22 @@ RSpec.describe TrelloApi do
   end
 
   describe "#create_list" do
-    pending
+    it "sends a POST request to create a list with their oAuth token, based on the board ID and specified list name" do
+      stub_request(:post, "#{TrelloApi.base_uri}/boards/trello-board-id/lists?key=#{ENV['TRELLO_KEY']}&token=trello-oauth-key")
+        .with(body: { name: "New List" })
+      trello = TrelloApi.new("trello-oauth-key")
+      trello.create_list("trello-board-id", "New List")
+    end
   end
 
   describe "#create_card" do
-    pending
+    it "sends a POST request to create a card with their oAuth token, based on the list ID and specified card info" do
+      card_info = { title: "Card Title", description: "Card Description" }
+
+      stub_request(:post, "#{TrelloApi.base_uri}/lists/trello-list-id/cards?key=#{ENV['TRELLO_KEY']}&token=trello-oauth-key")
+        .with(body: card_info )
+      trello = TrelloApi.new("trello-oauth-key")
+      trello.create_card("trello-list-id", card_info)
+    end
   end
 end
