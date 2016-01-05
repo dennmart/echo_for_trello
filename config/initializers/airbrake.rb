b5616de6484dbbeaf1a4fbda2659f7d000ed1682
Dennis Martinez <dennis@dennmart.com>
2016-01-06 08:55:40 +0900

    Configure Airbrake to ignore development and test environments

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,6 @@
 Airbrake.configure do |config|
-  config.project_key = ENV['ECHO_TRELLO_AIRBRAKE_API']
-  config.project_id  = true
-  config.host        = ENV['ECHO_TRELLO_AIRBRAKE_HOST']
+  config.project_key         = ENV['ECHO_TRELLO_AIRBRAKE_API']
+  config.project_id          = true
+  config.host                = ENV['ECHO_TRELLO_AIRBRAKE_HOST']
+  config.ignore_environments = %w(development test)
 end
