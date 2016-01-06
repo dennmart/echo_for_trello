65b3110b9330942deb6d6fbe2dc3d6cf998b245e
Dennis Martinez <dennis@dennmart.com>
2016-01-06 17:28:40 +0900

    Set defaults for Airbrake configurations if environment vars not set
    These are bogus defaults but it will at least let the app run, without
    needing to specify environment variables.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
-  config.project_key         = ENV['ECHO_TRELLO_AIRBRAKE_API']
+  config.project_key         = ENV['ECHO_TRELLO_AIRBRAKE_API'] || "airbrake-api-key"
   config.project_id          = true
-  config.host                = ENV['ECHO_TRELLO_AIRBRAKE_HOST']
+  config.host                = ENV['ECHO_TRELLO_AIRBRAKE_HOST'] || "https://test.airbrake.io/"
   config.ignore_environments = %w(development test)
 end
