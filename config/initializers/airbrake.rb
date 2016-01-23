4bcc8032ee58f0350362e50c3098b9891b8ba9d7
Dennis Martinez <dennis@dennmart.com>
2016-01-23 15:47:39 +0900

    Setting environment in Airbrake configuration
    This is necessary if we want to ignore errors in the environments
    specified by ignore_environments.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,5 +2,6 @@ Airbrake.configure do |config|
   config.project_key         = ENV['ECHO_TRELLO_AIRBRAKE_API'] || "airbrake-api-key"
   config.project_id          = true
   config.host                = ENV['ECHO_TRELLO_AIRBRAKE_HOST'] || "https://test.airbrake.io/"
+  config.environment         = Rails.env
   config.ignore_environments = %w(development test)
 end
