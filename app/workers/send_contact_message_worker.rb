6b99ca9136ea057c1dcde05005e865bd5925912a
Dennis Martinez <dennis@dennmart.com>
2016-04-21 16:43:46 +0900

    Symbolize keys from parameter to keep in sync throughout the code

--- app/workers/send_contact_message_worker.rb
+++ app/workers/send_contact_message_worker.rb
@@ -2,6 +2,7 @@ class SendContactMessageWorker
   include Sidekiq::Worker
 
   def perform(contact_params)
-    ContactMailer.send_contact_message(contact_params["name"], contact_params["email"], contact_params["message"]).deliver_now!
+    contact_params.symbolize_keys!
+    ContactMailer.send_contact_message(contact_params[:name], contact_params[:email], contact_params[:message]).deliver_now!
   end
 end
--- spec/workers/send_contact_message_worker_spec.rb
+++ spec/workers/send_contact_message_worker_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SendContactMessageWorker, type: :worker do
   describe "#perform" do
     it "sends an email with the specified parameters" do
       expect(ContactMailer).to receive(:send_contact_message).with("John Doe", "johndoe@test.com", "This is my message!").and_call_original
-      SendContactMessageWorker.new.perform({ name: "John Doe", email: "johndoe@test.com", message: "This is my message!" })
+      SendContactMessageWorker.new.perform({ "name" => "John Doe", "email" => "johndoe@test.com", "message" => "This is my message!" })
       expect(ActionMailer::Base.deliveries).to_not be_empty
     end
   end
