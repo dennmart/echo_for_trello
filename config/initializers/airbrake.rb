8e8f3a48efa5a86cd2738fafee337377ff065f62
Dennis Martinez <dennis@dennmart.com>
2016-07-04 16:24:39 +0900

    Fix the project_id setting for Airbrake
    This changed recently in Airbrake 1.4.1, so a project ID needs to be set
    properly.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
   config.project_key         = ENV['ECHO_TRELLO_AIRBRAKE_API'] || "airbrake-api-key"
-  config.project_id          = true
+  config.project_id          = ENV['ECHO_TRELLO_AIRBRAKE_PROJECT_ID'] || "airbrake-project-id"
   config.host                = ENV['ECHO_TRELLO_AIRBRAKE_HOST'] || "https://test.airbrake.io/"
   config.environment         = Rails.env
   config.ignore_environments = %w(development test)
