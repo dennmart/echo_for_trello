b2ff3dfd9d176ad7a1af4c29730f784730315d84
Dennis Martinez <dennis@dennmart.com>
2017-04-28 16:27:07 +0900

    Add account OAuth scope in order to get the user's email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :trello, ENV['TRELLO_KEY'], ENV['TRELLO_SECRET'],
-    app_name: "Echo for Trello", scope: 'read,write', expiration: 'never'
+    app_name: "Echo for Trello", scope: 'read,write,account', expiration: 'never'
 end
