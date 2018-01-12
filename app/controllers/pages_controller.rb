1ff7474b8904f2575350fb1026fe77d5973a681e
Dennis Martinez <dennis@dennmart.com>
2018-01-12 15:42:32 +0900

    Don't redirect if already authenticated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,9 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
-  before_action :redirect_if_authenticated, only: [:home]
   layout 'static'
 
   private
-
-  def redirect_if_authenticated
-    if session[:user_id]
-      redirect_to boards_path
-    end
-  end
 
   def layout_for_page
     case params[:id]
