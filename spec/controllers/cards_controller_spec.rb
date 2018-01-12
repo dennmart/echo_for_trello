0886567360aa482006f280c931f5f144cadd67fa
Dennis Martinez <dennis@dennmart.com>
2018-01-12 15:25:14 +0900

    Added alert to let users know about IFTTT

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -8,17 +8,18 @@ RSpec.describe CardsController, :type => :controller do
   end
 
   describe "#index" do
-    it "fetches the current user's cards" do
+    it "fetches the current user's enabled cards" do
       new_user = FactoryGirl.create(:user)
       user_card = FactoryGirl.create(:card, user: user)
       new_user_card = FactoryGirl.create(:card, user: new_user)
+      disabled_card = FactoryGirl.create(:card, user: user, disabled: true)
 
       boards = double
       expect_any_instance_of(TrelloApi).to receive(:boards).and_return(boards)
 
       get :index
       expect(assigns(:cards)).to include(user_card)
-      expect(assigns(:cards)).to_not include(new_user_card)
+      expect(assigns(:cards)).to_not include(new_user_card, disabled_card)
       expect(assigns(:boards)).to eq(boards)
     end
   end
