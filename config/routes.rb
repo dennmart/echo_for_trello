4a43ccc73bb615de559bb66c24ae1f6803d49700
Dennis Martinez <dennis@dennmart.com>
2018-01-12 15:29:31 +0900

    Remove all routes that won't be used during shutdown period

--- config/routes.rb
+++ config/routes.rb
@@ -3,37 +3,10 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   root to: 'pages#home', id: 'home'
 
-  resources 'boards' do
-    member do
-      post 'new_list'
-    end
-  end
-
-  resources 'cards' do
-    member do
-      put 'update_status'
-    end
-
-    collection do
-      get 'logs'
-    end
-  end
-
-  resources 'settings', only: [:index, :update]
-  resources 'contact', only: [:index, :create]
+  resources 'cards', only: [:index]
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
   get '/logout', to: 'sessions#destroy', as: 'logout'
   get '/admin', to: 'admin#index'
-
-  get '/faq', to: 'pages#faq'
-  get '/terms_and_conditions', to: 'pages#terms_and_conditions'
-  get '/privacy_policy', to: 'pages#privacy_policy'
-
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
-  end if Rails.env.production?
-
-  mount Sidekiq::Web => '/sidekiq'
 end
